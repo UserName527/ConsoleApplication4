102c2a3d87d75a3e6bf205ba4398fd079bf4d04d
Atos <70628905+ireadyium@users.noreply.github.com>
2023-09-28 09:30:31 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <format>
+
+int find_time(int time)
+{
+	int seconds = time % 60;
+	int minutes = time / 60;
+	int hours = minutes / 60;
+	minutes = minutes % 60;
+	std::cout << std::format("{}:{}:{}",hours, minutes, seconds);
+	return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int second_time;
+	std::cout << "Введить время в секундах: ";
+	std::cin >> second_time;
+	find_time(second_time);
+
+}
